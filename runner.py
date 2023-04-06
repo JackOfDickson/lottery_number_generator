bdd6d4dd749a4897d59c6a8f54334319a66dcf48
Jack Dickson <98648241+JackOfDickson@users.noreply.github.com>
2023-04-06 22:04:30 +0100

    made function for checking if a generated lottery ticket are winning numbers

--- runner.py
+++ runner.py
@@ -1,4 +1,9 @@
 from src.lottery_number_generator import *
+from src.lottery_winning_ticket_checker import *
 
 my_numbers = lotto_number_generator()
 print(my_numbers)
+
+winning_lotto_numbers = [4, 18, 29, 30, 39, 52, 46]
+result = lotto_winning_ticket_checker(my_numbers, winning_lotto_numbers)
+print(result)
