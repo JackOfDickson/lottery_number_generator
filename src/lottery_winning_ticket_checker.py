c5152b4e17aac1d303489ae7a36bc756b6e48494
Jack Dickson <jackowendickson@gmail.com>
2023-04-11 17:54:18 +0100

    refactored the ticket checker so that it checks if it matches the bonus ball seperately, will need to test this

--- src/lottery_winning_ticket_checker.py
+++ src/lottery_winning_ticket_checker.py
@@ -1,9 +1,20 @@
 def lotto_winning_ticket_checker( ticket_numbers, winning_numbers):
     matching_number_count = 0
+    bonus_ball = winning_numbers.pop() #Bonus Ball last number in the list
+    non_matching_numbers = [] #To be used to check if they match the bonus ball
+    matching_bonus_ball = False
 
-    for winning_number in winning_numbers:
-        if winning_number in ticket_numbers:
+    for number in ticket_numbers:
+        if number in winning_numbers:
             matching_number_count += 1
+        else:
+            non_matching_numbers.append(number)
+
+    
+    for number in non_matching_numbers:
+        if number == bonus_ball:
+            matching_bonus_ball = True
+            break
 
     if matching_number_count == 2:
         return "You have won a lucky dip!"
